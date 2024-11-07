441964008db1a0dd23134a8dd5d39be4b2fc3d84
Nathan Orlowski <nathan@Someone.local>
2024-11-07 16:39:20 +0200

    change name order

--- Bill3.java
+++ Bill3.java
@@ -12,6 +12,6 @@ public class Bill3 {
 
 		double finalSum = Math.ceil((double) sum / 3);
 
-		System.out.println("Dear  " + name1 + ", " + name2 + " and " + name3 + ": pay " + finalSum + " Shekel each.");
+		System.out.println("Dear  " + name3 + ", " + name2 + " and " + name1 + ": pay " + finalSum + " Shekel each.");
 	}
 }
