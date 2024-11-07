33412508e935885d77c5bd7343a0ca4a92dfdd7e
Nathan Orlowski <nathan@Someone.local>
2024-11-07 16:12:26 +0200

    completed

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,13 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+
+		int hundreds = num / 100; 
+		int tens = (num%100)/10;
+		int ones = num%10;
+
+		System.out.println(hundreds + " hundreds, " + tens +  " tens, and " + ones + " ones." );
+
 	}
 }
